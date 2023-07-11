c95f114d5411d4f8577dc70c9f151a8134dfa4c9
unknown <p.z.palasti@gmail.com>
2023-07-11 11:22:03 +0200

    hour log

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,9 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
+
 import "~/styles/globals.css";
+import '../components/Calendar/Calendar.css'
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
